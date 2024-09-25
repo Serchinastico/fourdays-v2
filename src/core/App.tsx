21d243586a2df03e0db6546c9d81822bbbebe792
Serchinastico <github@serchinastico.com>
2024-09-25 11:42:38 +0200

    fix: tweak Android status bar

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,3 +1,5 @@
+import { StatusBar } from "expo-status-bar";
+
 import { bootstrap } from "./bootstrap";
 import RootNavigation from "./navigation/RootNavigation";
 import AllProviders from "./providers/AllProviders";
@@ -7,6 +9,8 @@ bootstrap();
 export default function App() {
   return (
     <AllProviders>
+      <StatusBar style="dark" />
+
       <RootNavigation />
     </AllProviders>
   );
