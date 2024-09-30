ed6267f609092a3860ece11d37c64c81418f8be7
Serchinastico <github@serchinastico.com>
2024-09-30 21:05:09 +0200

    fix: force the usage of one of the provided locales or english as the default

--- src/core/bootstrap/translations.ts
+++ src/core/bootstrap/translations.ts
@@ -3,7 +3,16 @@ import { messages as esMessages } from "@app/locales/es/messages";
 import { i18n } from "@lingui/core";
 import * as Localization from "expo-localization";
 
+const LOCALES = { en: enMessages, es: esMessages };
+const ALLOWED_LOCALES = Object.keys(LOCALES);
+
+Object.entries(LOCALES).forEach(([locale, messages]) => {
+  i18n.load(locale, messages);
+});
+
 const locales = Localization.getLocales().map((locale) => locale.languageCode);
-i18n.load("en", enMessages);
-i18n.load("es", esMessages);
-i18n.activate(locales[0] ?? "en");
+const locale = locales.find(
+  (locale) => locale && ALLOWED_LOCALES.includes(locale)
+);
+
+i18n.activate(locale ?? "en");
